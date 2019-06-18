303c13c49ff2e34e4b4e1c393293ce5f4adae0e0
Devanm15 <you@example.com>
2019-06-18 22:36:08 +0000

    finished feature/add_to_cart_spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
+    
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -11,19 +12,20 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
         quantity: 10,
         price: 64.99
       )
+    
   end
-  scenario "They see product detail page" do
+
+  scenario "The cart increases when the add button is clicked" do
     # ACT
     visit root_path
-    page.find('article.product footer').first(:link,"Details").click
-   
 
+    first(:button, "Add").click
 
     # DEBUG
     # save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product-detail', 
+    within('nav') { expect(page).to have_content('My Cart (1)') }
     sleep(2)
     save_screenshot
     puts page.html
